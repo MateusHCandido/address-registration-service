d61c4e3c042c0472b1f19b289b67bc5045abe1da
Mateus Henrique <mateushcandido5@gmail.com>
2023-01-30 18:48:05 -0300

    Fase inicial do projeto

---
+++ src/main/java/com/mtzz/api/PersonManagementApiApplication.java
@@ -0,0 +1,14 @@
+package com.mtzz.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PersonManagementApiApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(PersonManagementApiApplication.class, args);
+	}
+}
